eedfbbaea55510cc1b2f4efa93553a7442c065d1
Andreas Fuchs <asf@boinkor.net>
2013-01-19 20:11:53 -0800

    Delurk the erlang compile task for now - unused, slow, noisy.

--- mix.exs
+++ mix.exs
@@ -1,35 +1,35 @@
-defmodule Mix.Tasks.Compile.Erlang do
-  use Mix.Task
+# defmodule Mix.Tasks.Compile.Erlang do
+#   use Mix.Task
 
-  @hidden true
-  @shortdoc "Compile Erlang source files"
+#   @hidden true
+#   @shortdoc "Compile Erlang source files"
 
-  def run(_) do
-    project      = Mix.project
-    compile_exts = [:erl]
-    to_compile = Mix.Utils.extract_files(["imap_parser"], compile_exts)
+#   def run(_) do
+#     project      = Mix.project
+#     compile_exts = [:erl]
+#     to_compile = Mix.Utils.extract_files(["imap_parser"], compile_exts)
 
-    compile_files(to_compile, project[:compile_path])
-  end
+#     compile_files(to_compile, project[:compile_path])
+#   end
 
-  defp compile_files(to_compile, compile_path) do
-    compile_one = function do
-                    f ->
-                      IO.puts "Compiling #{f}"
-                      {:ok, _} = :compile.file(Kernel.binary_to_list(f))
-                  end
-    Enum.each to_compile, compile_one
-  end
-end
+#   defp compile_files(to_compile, compile_path) do
+#     compile_one = function do
+#                     f ->
+#                       IO.puts "Compiling #{f}"
+#                       {:ok, _} = :compile.file(Kernel.binary_to_list(f))
+#                   end
+#     Enum.each to_compile, compile_one
+#   end
+# end
 
-defmodule Mix.Tasks.Compile.Imapparse do
-  @shortdoc "Compiles the imap parser and lexer"
+# defmodule Mix.Tasks.Compile.Imapparse do
+#   @shortdoc "Compiles the imap parser and lexer"
 
-  def run(_) do
-    IO.puts "Compiling Lexer imap.peg"
-    :ok = :neotoma.file('imap_parser/imap.peg')
-  end
-end
+#   def run(_) do
+#     IO.puts "Compiling Lexer imap.peg"
+#     :ok = :neotoma.file('imap_parser/imap.peg')
+#   end
+# end
 
 defmodule GmailSynchronize.Mixfile do
   use Mix.Project
@@ -38,7 +38,7 @@ defmodule GmailSynchronize.Mixfile do
     [ app: :gmail_synchronize,
       version: "0.0.1",
       deps: deps,
-      compilers: [:erlang, :elixir, :app],
+      compilers: [:elixir, :app],
       source_paths: ["lib"]
     ]
   end
@@ -51,6 +51,8 @@ defmodule GmailSynchronize.Mixfile do
   # Returns the list of dependencies in the format:
   # { :foobar, "0.1", git: "https://github.com/elixir-lang/foobar.git" }
   defp deps do
-    [{:neotoma, "1.5.1", git: "https://github.com/seancribbs/neotoma.git"}]
+    [
+     #{:neotoma, "1.5.1", git: "https://github.com/seancribbs/neotoma.git"}
+    ]
   end
 end
