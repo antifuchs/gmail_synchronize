a850abdf241133b4836d44cd432734c68e679e74
Andreas Fuchs <asf@boinkor.net>
2013-04-21 19:30:23 -0700

    Remove all the silly custom compiler action
    Mix can now compile erlang and leex/yecc files out of the box!

--- mix.exs
+++ mix.exs
@@ -1,46 +1,10 @@
-# defmodule Mix.Tasks.Compile.Erlang do
-#   use Mix.Task
-
-#   @hidden true
-#   @shortdoc "Compile Erlang source files"
-
-#   def run(_) do
-#     project      = Mix.project
-#     compile_exts = [:erl]
-#     to_compile = Mix.Utils.extract_files(["imap_parser"], compile_exts)
-
-#     compile_files(to_compile, project[:compile_path])
-#   end
-
-#   defp compile_files(to_compile, compile_path) do
-#     compile_one = function do
-#                     f ->
-#                       IO.puts "Compiling #{f}"
-#                       {:ok, _} = :compile.file(Kernel.binary_to_list(f))
-#                   end
-#     Enum.each to_compile, compile_one
-#   end
-# end
-
-# defmodule Mix.Tasks.Compile.Imapparse do
-#   @shortdoc "Compiles the imap parser and lexer"
-
-#   def run(_) do
-#     IO.puts "Compiling Lexer imap.peg"
-#     :ok = :neotoma.file('imap_parser/imap.peg')
-#   end
-# end
-
 defmodule GmailSynchronize.Mixfile do
   use Mix.Project
 
   def project do
     [ app: :gmail_synchronize,
       version: "0.0.1",
-      deps: deps,
-      compilers: [:elixir, :app],
-      source_paths: ["lib"]
-    ]
+      deps: deps ]
   end
 
   # Configuration for the OTP application
@@ -51,8 +15,6 @@ defmodule GmailSynchronize.Mixfile do
   # Returns the list of dependencies in the format:
   # { :foobar, "0.1", git: "https://github.com/elixir-lang/foobar.git" }
   defp deps do
-    [
-     #{:neotoma, "1.5.1", git: "https://github.com/seancribbs/neotoma.git"}
-    ]
+    []
   end
 end
